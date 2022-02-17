738d00dac8adc620238a0d894552a1042f2b979e
Eduardo Akira H. Oushiro <akiraeduu@gmail.com>
2022-02-17 12:38:38 -0300

    Criação da estrutura da home

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:nubank_figma/home/containers/container_cartao_credito.dart';
+import 'package:nubank_figma/home/containers/container_conta.dart';
+import 'package:nubank_figma/home/containers/container_rewards.dart';
+
+import 'containers/container_emprestimo.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -6,9 +11,35 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        title: Text("Home Page"),
+        backgroundColor: Colors.black,
+        elevation: 0,
+        // ignore: prefer_const_constructors
+        title: Text(
+          "Olá, Felipe",
+          // ignore: prefer_const_constructors
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold
+          ),
+          ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Column(
+            children: [
+              ContainerCartaoCredito(),
+              ContainerConta(), 
+              ContainerEmprestimo(),
+              ContainerRewards()
+            ],
+            ),
+        ),
       ),
     );
   }
 }
+
+// Usa Padding para espaçamento
