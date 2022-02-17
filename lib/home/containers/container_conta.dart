3ac80c06a45bfde740634aeb384c7d7b62aa28e3
Eduardo Akira H. Oushiro <akiraeduu@gmail.com>
2022-02-17 15:45:29 -0300

    Criação do container conta

--- lib/home/containers/container_conta.dart
+++ lib/home/containers/container_conta.dart
@@ -14,6 +14,41 @@ class ContainerConta extends StatelessWidget {
           borderRadius: BorderRadius.circular(5),
           color: const Color(0xFF121212),
         ),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 40, bottom: 14),
+                child: Row(
+                  children: [
+                    Image.asset("images/cartao.png"),
+                    const SizedBox(width: 14),
+                    const Text(
+                      "Conta",
+                      style: TextStyle(
+                        color: Colors.white
+                      ),
+                      ),
+                  ],
+                  ),
+              ),
+              const Text("Saldo disponível",
+              style: TextStyle(color: Color(0xFF9E9C9C)),
+              ),
+              const SizedBox(height: 16,),
+              const Text("R\$ 121,21",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 28
+              ),
+              ),
+              const SizedBox(height: 41,)
+            ],
+          ),
+        ),
       ),
     );
   }
