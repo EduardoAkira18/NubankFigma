f819d7c624c1765e7e9a099c43cb5f59c2fc883a
Eduardo Akira H. Oushiro <akiraeduu@gmail.com>
2022-02-17 16:40:02 -0300

    Criação do Container Emprestimo

--- lib/home/containers/container_emprestimo.dart
+++ lib/home/containers/container_emprestimo.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ContainerEmprestimo extends StatelessWidget {
@@ -13,6 +16,65 @@ class ContainerEmprestimo extends StatelessWidget {
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(5),
           color: const Color(0xFF121212),
+        ),
+        child: Padding(
+          padding: EdgeInsets.only(left: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 22, bottom: 14),
+                child: Row(
+                  children: [
+                    Image.asset("images/cartao.png"),
+                    const SizedBox(
+                      width: 14,
+                    ),
+                    const Text(
+                      "Empréstimo",
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ],
+                ),
+              ),
+              const Text(
+                "Valor disponível até",
+                style: TextStyle(color: Color(0xFF9E9C9C)),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              const Text(
+                "R\$ 5.000,00",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.transparent,
+                  borderRadius: BorderRadius.circular(4),
+                  border: Border.all(color: Colors.white),
+                ),
+                width: 217,
+                height: 41,
+                child: const Center(
+                  child: Text(
+                    "SIMULAR EMPRESTIMO",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
